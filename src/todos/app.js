e7b8efde6bb1241dd69955fdd47de09aa0b9cd16
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2023-03-09 16:38:28 -0300

    filter changed

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,4 +1,4 @@
-import todoStore from "../store/todo.store";
+import todoStore, { Filters } from "../store/todo.store";
 import html from "./app.html?raw";
 import { renderTodos } from "./use-cases";
 
@@ -6,6 +6,7 @@ const ElementIDs = {
   ClearCompletedButton: ".clear-completed",
   TodoList: ".todo-list",
   NewTodoInput: "#new-todo-input",
+  TodoFilters: ".filtro",
 };
 
 /**
@@ -33,6 +34,7 @@ export const App = (elementId) => {
   const clearCompletedButton = document.querySelector(
     ElementIDs.ClearCompletedButton
   );
+  const filtersLIs = document.querySelectorAll(ElementIDs.TodoFilters);
 
   // Listeners
   newDescriptionInput.addEventListener("keyup", (event) => {
@@ -63,5 +65,27 @@ export const App = (elementId) => {
   clearCompletedButton.addEventListener("click", () => {
     todoStore.deleteCompleted();
     displayTodos();
+  });
+
+  filtersLIs.forEach((element) => {
+    element.addEventListener("click", (element) => {
+      filtersLIs.forEach((el) => el.classList.remove("selected"));
+      element.target.classList.add("selected");
+
+      switch (element.target.text) {
+        case "Todos":
+          todoStore.setFilter(Filters.All);
+          break;
+        case "Pendientes":
+          todoStore.setFilter(Filters.Pending);
+          break;
+        case "Completados":
+          todoStore.setFilter(Filters.Completed);
+          break;
+        default:
+          break;
+      }
+      displayTodos();
+    });
   });
 };
